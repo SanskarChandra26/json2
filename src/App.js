f2524a07bc431de8abd26202a96008e6a732f271
SanskarChandra26 <76607315+SanskarChandra26@users.noreply.github.com>
2023-02-09 01:53:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [json, setJson] = useState("");
+	useEffect(() => {
+		async function getData() {
+			const response = await fetch("/openapi2.json");
+			const json1 = await response.json();
+			setJson(...json, json1);
+			console.log(json1);
+		}
+		getData();
+	}, []);
+	return <div>{JSON.stringify(json)}</div>;
 }
 
 export default App;
