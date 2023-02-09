75b83307c6c252db36f94a9b02b237078e1e2144
SanskarChandra26 <76607315+SanskarChandra26@users.noreply.github.com>
2023-02-09 09:41:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-	const [json, setJson] = useState("");
+	// const config = {
+	// 	headers: {
+	// 		accept: "application/json",
+	// 	},
+	// 	data: {},
+	// };
+	const [json, setJson] = useState([]);
 	useEffect(() => {
 		async function getData() {
-			const response = await fetch("/openapi2.json");
+			const response = await fetch("http://localhost:3000/posts");
 			const json1 = await response.json();
 			setJson(...json, json1);
 			console.log(json1);
